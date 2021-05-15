5075bf623799176c741652d1840c1b03d590eefe
Selesito <selesitomorozov@gmail.com>
2021-05-15 09:08:57 +0400

    1. PsqlStore. [#522677]

---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,38 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+import java.sql.SQLException;
+
+public class PsqlMain {
+    public static void main(String[] args) throws SQLException {
+        Store store = PsqlStore.instOf();
+        store.save(new Post(0, "Java Job"));
+        store.save(new Post(0, "C Job"));
+        store.save(new Post(0, "C++ Job"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        System.out.println();
+        store.save(new Candidate(0, "Junior Java"));
+        store.save(new Candidate(0, "Middle Java"));
+        store.save(new Candidate(0, "Senior Java"));
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        System.out.println();
+        store.save(new Post(2, "C Job изменено"));
+        store.save(new Candidate(1, "Junior Java изменено"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        System.out.println();
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        System.out.println();
+        System.out.println(store.findByIdPost(2).getName());
+        System.out.println(store.findByIdCandidate(1).getName());
+    }
+}
